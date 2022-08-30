1461d5cb9bc52e44cb61a4c6d739acfacb4785bd
Emmafan <emmafan1114@gmail.com>
2022-08-30 11:25:54 +0800

    全站scss, css設定

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.scss';
-
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <button className="btn btn-primary m-2">按鈕</button>
+        <button className="btn btn-secondary m-2">按鈕</button>
+        <button className="btn btn-danger m-2">按鈕</button>
+        <h2>這是h2標題</h2>
+        <h3>這是h3標題</h3>
+        <h4>這是h4標題</h4>
+        <h5>這是h5標題</h5>
+        <h6>這是h6標題</h6>
+        <p>文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p</p>
+        <p className="small">
+          文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="minimum">
+          文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p
+        </p>
+        <div className="accent-light-color bg-gary-dark-color w-50 p-5">
+          <p>
+            文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p文字p
+          </p>
+        </div>
       </header>
     </div>
   );
