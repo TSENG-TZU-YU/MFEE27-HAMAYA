47443cf4c2cb3a57cfdc9b5dc32786c2ef9d15cf
Emmafan <emmafan1114@gmail.com>
2022-08-30 16:59:46 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.scss';
 
 // 版面頁面元件
-import MainOutlet from './layouts/MainOutlet'
+import MainOutlet from './layouts/MainOutlet';
 
 // 其它子頁面
 import Home from './pages/Home';
@@ -29,9 +29,9 @@ function App() {
             {/* 注意Product頁面在 /product/index.js 裡，只是一個作layout用頁面*/}
 
             {/* index代表此路由中的預設子頁 */}
-              {/* <Route index element={<List />} /> */}
-              {/* 代表此路由中的有id子頁，例`product/123` */}
-              {/* <Route path=":productId" element={<Detail />} /> */}
+            {/* <Route index element={<List />} /> */}
+            {/* 代表此路由中的有id子頁，例`product/123` */}
+            {/* <Route path=":productId" element={<Detail />} /> */}
             <Route path="product" element={<MainOutlet />}>
               <Route index element={<Products />} />
             </Route>
@@ -59,7 +59,7 @@ function App() {
             <Route path="Members" element={<MainOutlet />}>
               <Route index element={<Members />} />
             </Route>
-            
+
             {/* 404未找到的頁面路由，需放在最下方 */}
             <Route path="*" element={<h1>404 NOT FOUND</h1>} />
           </Route>
