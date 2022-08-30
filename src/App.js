3162de0a148179cb21828b5e5c44a6d40f5f1f37
Emmafan <emmafan1114@gmail.com>
2022-08-30 14:10:53 +0800

    scss css 修改

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button className="btn btn-primary m-2">按鈕</button>
-        <button className="btn btn-secondary m-2">按鈕</button>
-        <button className="btn btn-danger m-2">按鈕</button>
+        <button className="btn btn-secondary m-5">按鈕</button>
+        <button className="btn btn-danger m-1">按鈕</button>
         <h2>這是h2標題</h2>
         <h3>這是h3標題</h3>
         <h4>這是h4標題</h4>
