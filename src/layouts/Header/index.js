ed7b8de5fdfaf15f74ae1363b6b433eab0ab6467
Lily <lilyhuang841020@gmail.com>
2022-08-31 17:09:46 +0800

    index.js更新

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,32 +1,45 @@
 import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Logo from '../../assets/HeaderImg/logo.svg';
+import Users from '../../assets/HeaderImg/users.svg';
+import Cart from '../../assets/HeaderImg/shopping_cart.svg';
+import LogoOut from '../../assets/HeaderImg/logout.svg';
+import '../../styles/global.scss';
 
 function Header(props) {
   return (
     <>
-      <div className="d-flex justify-content-start align-items-end ">
-        <Link className="mx-3" to="/">
-          Home
+      <Link className="mx-3" to="/">
+        <img src={Logo} width="160" alt="Logo" className="mr-2" />
+      </Link>
+      <Container>
+        <div className="d-flex justify-content-center align-items-end ">
+          <Link className="mx-3" to="news">
+            最新消息
+          </Link>
+          <Link className="mx-3" to="product">
+            樂器商城
+          </Link>
+          <Link className="mx-3" to="class">
+            音樂教育
+          </Link>
+          <Link className="mx-3" to="place">
+            場地租借
+          </Link>
+          <Link className="mx-3" to="aboutus">
+            關於我們
+          </Link>
+        </div>
+      </Container>
+      <div className="d-flex justify-content-end align-items-end ">
+        <Link className="mx-3" to="">
+          <img src={Cart} width="25" alt="Logo" className="mr-2" />
         </Link>
-        <Link className="mx-3" to="NEWs">
-          NEWs
+        <Link className="mx-3" to="members">
+          <img src={Users} width="25" alt="Logo" className="mr-2" />
         </Link>
-        <Link className="mx-3" to="product">
-          Product
-        </Link>
-        <Link className="mx-3" to="Class">
-          Class
-        </Link>
-        <Link className="mx-3" to="Educate">
-          Educate
-        </Link>
-        <Link className="mx-3" to="Place">
-          Place
-        </Link>
-        <Link className="mx-3" to="AboutUs">
-          AboutUs
-        </Link>
-        <Link className="mx-3" to="Members">
-          Members
+        <Link className="mx-3" to="">
+          <img src={LogoOut} width="25" alt="Logo" className="mr-2" />
         </Link>
       </div>
       <hr />
