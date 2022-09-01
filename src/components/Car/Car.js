4276f849c7a43b2e4cecf5323d57dee0722846f6
Joy <q39078273@gmail.com>
2022-09-01 16:36:21 +0800

    123

---
+++ src/components/Car/Car.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Car.scss';
+
+// 圖檔
+import shop_car from '../../assets/svg/add_shopping_cart.svg';
+
+function Car(props) {
+  return (
+    <div className="add-car d-flex justify-content-center align-items-center">
+      <img className="me-2" src={shop_car} alt="shop_car" />
+      <small>加入購物車</small>
+    </div>
+  );
+}
+
+export default Car;
