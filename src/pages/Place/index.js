19cbc2198aa7eb0070e6c1a1c54afb260d7814f6
Steven <a77563166@yahoo.com.tw>
2022-09-01 17:17:26 +0800

    fix place樣式

--- src/pages/Place/index.js
+++ src/pages/Place/index.js
@@ -1,14 +1,22 @@
-import { Container, Row, Col, Form } from 'react-bootstrap';
-
 import React from 'react';
 import './place.scss';
 import banner from '../../assets/PlaceImg/banner.png';
+import studioa1 from '../../assets/PlaceImg/studioa1.png';
+import studioa2 from '../../assets/PlaceImg/studioa2.png';
+import studioa3 from '../../assets/PlaceImg/studioa3.png';
+import studioc1 from '../../assets/PlaceImg/studioc1.png';
+import studioc2 from '../../assets/PlaceImg/studioc2.png';
+import studioc3 from '../../assets/PlaceImg/studioc3.png';
+import studiob1 from '../../assets/PlaceImg/studiob1.png';
+import studiob2 from '../../assets/PlaceImg/studiob2.png';
+import studiob3 from '../../assets/PlaceImg/studiob3.png';
+import studiob4 from '../../assets/PlaceImg/studiob3.png';
 
 function Place(props) {
   return (
     <>
       <img src={banner} alt="banner" />
-      <Container>
+      <div className="container">
         <div className="d-flex mt-5">
           <a href="/">
             <p>首頁</p>
@@ -19,104 +27,110 @@ function Place(props) {
             <p>場地租借</p>
           </a>
         </div>
-        <div className="d-flex blank-top ">
-          <h4 className="" style={{ color: '#00323d' }}>
-            租借項目
-          </h4>
-          <h4 className="engText" style={{ color: '#00323d' }}>
-            RENTAL ITEM
-          </h4>
-          <div className="vector3  mt-3"></div>
-        </div>
-
-        <h4 className="item1 d-flex justify-content-end pt-5 pb-2">
-          Studio-A 錄音室
-        </h4>
-        <div className="intro1 p-5 justify-content-end">
-          <p>
-            <p>
-              Studio-A
-              錄音室，深知每個音樂人都有成名夢，這裡提供給音樂人一個專業錄音空間，將自己的作品完美的輸出成輯。
-            </p>
-            <h6>設備介紹</h6>
-            <p> 8軌A類等級的麥克風前極放大器 </p>
-            <p>3軌A類等級的Di </p>
-            <p>4軌B類等級的麥克風放大器</p>
-            <p>ADAD類比數位轉換器</p>
-            <h6>時段 08:00 - 21:00　平日 NT $2,000　假日 NT $2,500</h6>
-          </p>
-        </div>
-
-        <div className="container">
-          <h4 className="item2">Studio-B 練團室</h4>
-          <div className="intro2 p-5">
-            <p>
-              精緻練團室出租，希望藉由完善、貼心的服務，使樂團能心無旁鶩的修練音樂技巧。
-            </p>
-            <h6>設備介紹</h6>
-            <p>練團室坪數：5坪 / 容納約7人 </p>
-            <p>吉他音箱：Laney IRT15H + Laney 412cab</p>
-            <p>吉他音箱：Marshall DSL15H + MX212cab</p>
-            <p>貝斯音箱：GK MB112 II </p>
-            <p> 爵士鼓組：Dixon Aritisan Standard 5粒組</p>
-            <h6>時段 08:00 - 21:00　平日 NT $2,000　假日 NT $2,500</h6>
-          </div>
-        </div>
-        <div className="container">
-          <h4 className="item3">Studio-C 小型展演空間</h4>
-          <div className="intro3 p-5 justify-content-end">
-            <p>
-              適合舉辦小型音樂展演、各類型講座、小型企業開會、產品活動發表會、各類型講座、私人包場與聚會。
-            </p>
-            <h6>設備介紹</h6>
-            <p>免費提供 300M/100M光纖 Wifi</p>
-            <p>100吋單槍投影幕、無線麥克風、頂級 LD System 音響系統</p>
-            <p>40張折疊椅、6張折疊桌、休息室、茶水間及衛生間</p>
-            <p>Yamaha C2 平台演奏琴</p>
-            <h6>時段 08:00 - 21:00　平日 NT $2,000　假日 NT $2,500</h6>
-          </div>
-        </div>
-      </Container>
-      <div className="session1-bg">
-        <Container>
-          <div className="d-flex pt-5">
-            <h4 className="" style={{ color: '#333333' }}>
-              場地預約
+        <div className="row">
+          <div className="col-3 mt-2">
+            <h4 className="" style={{ color: '#00323d' }}>
+              租借項目<span className="engText">RENTAL ITEM</span>
             </h4>
-            <h4 className="engText" style={{ color: '#333333' }}>
-              VENUE RESERVATION
-            </h4>
-            <div className="vector3  mt-3"></div>
           </div>
-          <Form>
-            <p className="formitem">姓名</p>
-            <input type="text" id="fullName" placeholder="請輸入姓名" />
-
-            <p className="formitem">聯絡電話</p>
-            <input type="phone" id="phone" placeholder="請輸入電話/手機" />
-
-            <p className="formitem">信箱</p>
-            <input type="mail" id="mail" placeholder="請輸入信箱" />
-
-            <p className="formitem">租借項目</p>
-            <input type="text" id="item" placeholder="請輸入欲租借場地" />
-
-            <p className="formitem">留言</p>
-            <input type="text" id="comment" placeholder="留言內容" />
-
-            <p className="formitem">使用日期</p>
-            <input type="date" id="usedate" />
-
-            <p className="formitem">使用時間</p>
-            <input type="time" id="usetime" />
-
-            <p className="formitem">使用人數</p>
-            <input type="number" id="users" />
-            {/* 設定不可為0&負 */}
-
-            <button>確認送出</button>
-          </Form>
-        </Container>
+          <div className="vector3 mt-4 col-9"></div>
+        </div>
+        {/* studioa */}
+        <div className="row">
+          <div className="col-6 p-0">
+            <img src={studioa1} alt="studioa1" />
+          </div>
+          <div className=" col-6 p-0">
+            <h4 className="item1 d-flex justify-content-end">
+              Studio-A 錄音室
+            </h4>
+            <div className="intro1 position-relative">
+              <p>
+                Studio-A
+                錄音室，深知每個音樂人都有成名夢，這裡提供給音樂人一個專業錄音空間，將自己的作品完美的輸出成輯。
+              </p>
+              <h6>設備介紹</h6>
+              <p> 8軌A類等級的麥克風前極放大器 </p>
+              <p>3軌A類等級的Di </p>
+              <p>4軌B類等級的麥克風放大器</p>
+              <p>ADAD類比數位轉換器</p>
+              <h6>時段 08:00 - 21:00　平日 NT $2,000　假日 NT $2,500</h6>
+            </div>
+            <div>
+              <button className="border-0 my-2">
+                <img src={studioa2} alt="studioa2" />
+              </button>
+              <button className="border-0 my-2">
+                <img src={studioa3} alt="studioa3" />
+              </button>
+            </div>
+          </div>
+          <hr className="m-5" />
+        </div>
+        {/* studioB */}
+        <div className="row">
+          <div className=" col-6 p-0">
+            <h4 className="item2">Studio-B 練團室</h4>
+            <div className="intro2 position-relative">
+              <p>
+                精緻練團室出租，希望藉由完善、貼心的服務，使樂團能心無旁鶩的修練音樂技巧。
+              </p>
+              <h6>設備介紹</h6>
+              <p>練團室坪數：5坪 / 容納約7人 </p>
+              <p>吉他音箱：Laney IRT15H + Laney 412cab</p>
+              <p>吉他音箱：Marshall DSL15H + MX212cab</p>
+              <p>貝斯音箱：GK MB112 II </p>
+              <p> 爵士鼓組：Dixon Aritisan Standard 5粒組</p>
+              <h6>時段 08:00 - 21:00　平日 NT $2,000　假日 NT $2,500</h6>
+            </div>
+            <div className="d-flex justify-content-end me-5">
+              <button className="border-0 my-2">
+                <img src={studiob2} alt="studiob2" />
+              </button>
+              <button className="border-0 my-2">
+                <img src={studiob3} alt="studiob3" />
+              </button>
+              <button className="border-0 my-2">
+                <img src={studiob4} alt="studiob4" />
+              </button>
+            </div>
+          </div>
+          <div className="col-6 p-0">
+            <img src={studiob1} alt="studiob1" />
+          </div>
+        </div>
+        <hr className="m-5" />
+        {/* studioC */}
+        <div className="row">
+          <div className="col-6 p-0">
+            <img src={studioc1} alt="studioc1" />
+          </div>
+          <div className=" col-6 p-0">
+            <h4 className="item3 d-flex justify-content-end">
+              Studio-C 小型展演空間
+            </h4>
+            <div className="intro3 position-relative">
+              <p>
+                適合舉辦小型音樂展演、各類型講座、小型企業開會、
+                產品活動發表會、各類型講座、私人包場與聚會。
+              </p>
+              <h6>設備介紹</h6>
+              <p>免費提供 300M/100M光纖 Wifi</p>
+              <p>100吋單槍投影幕、無線麥克風、頂級 LD System 音響系統</p>
+              <p>40張折疊椅、6張折疊桌、休息室、茶水間及衛生間</p>
+              <p>Yamaha C2 平台演奏琴</p>
+              <h6>時段 08:00 - 21:00　平日 NT $2,000　假日 NT $2,500</h6>
+            </div>
+            <div>
+              <button className="border-0 my-2">
+                <img src={studioc2} alt="studioc2" />
+              </button>
+              <button className="border-0 my-2">
+                <img src={studioc3} alt="studioc3" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
