cd6066a3b46e3d4d94a1cb226c90c28c6570f785
Emmafan <emmafan1114@gmail.com>
2022-09-02 11:11:29 +0800

    products classNema 修改

--- src/pages/Products/ProductsList/MobileFilterNav.js
+++ src/pages/Products/ProductsList/MobileFilterNav.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import './styles/MobileFilterNav.scss';
 
+// 圖檔
+import arrowDown from '../../../assets/ProductsImg/icon/arrow_down.svg';
+
 function MobileFilterNav() {
   return (
-    <div className="mobile-filter-nav">
+    <div className="mobile-filter-nav d-md-none">
       <div className="d-flex justify-content-center align-items-center">
-        <div className="filter-nav-item border-end">
-          <p className="filter-nav-item-name">商品分類</p>
-          <button className="filter-nav-btn p-2">琴鍵樂器</button>
+        <div className="products-filter-nav-item border-end">
+          <p className="products-filter-nav-item-name">商品分類</p>
+          <button className="products-btn-border-none products-filter-nav-btn p-2 d-flex align-items-center">
+            琴鍵樂器
+            <img src={arrowDown} alt="arrowDown" />
+          </button>
         </div>
-        <div className="filter-nav-item border-end">
-          <p className="filter-nav-item-name">進階篩選</p>
-          <button className="filter-nav-btn p-2">請選擇篩選條件</button>
+        <div className="products-filter-nav-item border-end">
+          <p className="products-filter-nav-item-name">進階篩選</p>
+          <button className="products-btn-border-none products-filter-nav-btn p-2 d-flex align-items-center">
+            篩選條件
+            <img src={arrowDown} alt="arrowDown" />
+          </button>
         </div>
-        <div className="filter-nav-item">
-          <p className="filter-nav-item-name">商品排序</p>
-          <button className="filter-nav-btn p-2">請選擇排序條件</button>
+        <div className="products-filter-nav-item">
+          <p className="products-filter-nav-item-name">商品排序</p>
+          <button className="products-btn-border-none products-filter-nav-btn p-2 d-flex align-items-center">
+            排序條件
+            <img src={arrowDown} alt="arrowDown" />
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import './index.scss';
+
+// 元件
 import CategoryNav from './ProductsList/CategoryNav';
 import ProductsItem from './ProductsList/ProductsItem';
 import FilterNav from './ProductsList/FilterNav';
 import MobileFilterNav from './ProductsList/MobileFilterNav';
+
+// 圖檔
 import banner from '../../assets/ProductsImg/banner.png';
 import bannerTitle from '../../assets/ProductsImg/icon/banner_title.svg';
 
 function index() {
   return (
     <>
-      <div className="banner-img position-relative">
+      <div className="products-banner-img position-relative">
         <img
           src={bannerTitle}
           alt="bannerTitle"
-          className="banner-title position-absolute"
+          className="products-banner-title position-absolute"
         />
         <img src={banner} alt="banner" />
       </div>
       <div className="container">
-        {/* <FilterNav /> */}
+        <FilterNav />
         <MobileFilterNav />
         {/* TODO: 再寫手機版元件做替換 */}
         <div className="row">
           {/* TODO: md以下讓它消失 */}
-          {/* <CategoryNav /> */}
+          <CategoryNav />
           <div className="col-12 col-md-10">
             <div className=" row row-cols-2 row-cols-md-4">
               <ProductsItem />
@@ -54,9 +58,9 @@ function index() {
             </div>
             {/* 頁碼 */}
             <div className="d-flex justify-content-center align-items-center flex-fill">
-              <div className="page my-3">
+              <div className="products-page my-3">
                 <a href="/">&#x3C;</a>
-                <a className="page-active" href="/">
+                <a className="products-page-active" href="/">
                   1
                 </a>
                 <a href="/">2</a>
