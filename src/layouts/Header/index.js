fca0e6e57566ed368147cfecc919092d5522aa12
Emmafan <emmafan1114@gmail.com>
2022-09-04 19:28:11 +0800

    compare, scrollTo

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -8,78 +8,100 @@ import LogoOut from '../../assets/HeaderImg/logout.svg';
 import './index.scss';
 
 import LogInSignUp from '../../components/LogInSignUp'; //睿渝加的CODE不要刪
+import ScrollTo from '../../components/ScrollTo';
 import Cart from '../Cart/Cart';
 import { Button } from 'bootstrap';
 
 function Header(props) {
-  const [loginPopup, setLoginPopup] = useState(false); //睿渝加的CODE不要刪
-  const [shoppingCart, setShoppingCart] = useState(false); //預設關閉
-  return (
-    <>
-      <div className="bg-main-gary-light-color">
-        <Container className="height ">
-          <div className="row">
-            <div className="col-3">
-              <Link className="" to="/">
-                <img src={Logo} width="180" alt="Logo" className="mr-2 mt-3" />
-              </Link>
-            </div>
-            <div className="col mt-5">
-              <Link className="mx-4 h6 " to="news">
-                最新消息
-              </Link>
-              <Link className="  mx-4 h6  " to="product">
-                樂器商城
-              </Link>
-              <Link className="  mx-4  h6" to="class">
-                音樂教育
-              </Link>
-              <Link className="mx-4   h6  " to="place">
-                場地租借
-              </Link>
-              <Link className="mx-4  h6" to="aboutus">
-                關於我們
-              </Link>
-            </div>
+    const [loginPopup, setLoginPopup] = useState(false); //睿渝加的CODE不要刪
+    const [shoppingCart, setShoppingCart] = useState(false); //預設關閉
+    return (
+        <>
+            <div className="bg-main-gary-light-color">
+                <Container className="height ">
+                    <div className="row">
+                        <div className="col-3">
+                            <Link className="" to="/">
+                                <img
+                                    src={Logo}
+                                    width="180"
+                                    alt="Logo"
+                                    className="mr-2 mt-3"
+                                />
+                            </Link>
+                        </div>
+                        <div className="col mt-5">
+                            <Link className="mx-4 h6 " to="news">
+                                最新消息
+                            </Link>
+                            <Link className="  mx-4 h6  " to="product">
+                                樂器商城
+                            </Link>
+                            <Link className="  mx-4  h6" to="class">
+                                音樂教育
+                            </Link>
+                            <Link className="mx-4   h6  " to="place">
+                                場地租借
+                            </Link>
+                            <Link className="mx-4  h6" to="aboutus">
+                                關於我們
+                            </Link>
+                        </div>
 
-            <div className="col-1 mt-4">
-              <div className="d-flex justify-content-end align-items-end ">
-                <button
-                  className="mx-3 border-0  btn "
-                  onClick={() => {
-                    shoppingCart
-                      ? setShoppingCart(false)
-                      : setShoppingCart(true);
-                  }}
-                >
-                  <img src={shopCart} width="25" alt="Logo" className="mr-2" />
-                </button>
-                {/*會員登入我改成按鈕 如果衝突 原本的會員LINK請刪除*/}
-                <button
-                  className="border-0 btn "
-                  onClick={() => {
-                    setLoginPopup(true);
-                  }}
-                >
-                  <img src={Users} width="25" alt="Logo" className="mr-2 " />
-                </button>
-                <button className="mx-3 border-0 btn  " to="">
-                  <img src={LogoOut} width="25" alt="Logo" className="mr-2 " />
-                </button>
-              </div>
-            </div>
-          </div>
+                        <div className="col-1 mt-4">
+                            <div className="d-flex justify-content-end align-items-end ">
+                                <button
+                                    className="mx-3 border-0  btn "
+                                    onClick={() => {
+                                        shoppingCart
+                                            ? setShoppingCart(false)
+                                            : setShoppingCart(true);
+                                    }}
+                                >
+                                    <img
+                                        src={shopCart}
+                                        width="25"
+                                        alt="Logo"
+                                        className="mr-2"
+                                    />
+                                </button>
+                                {/*會員登入我改成按鈕 如果衝突 原本的會員LINK請刪除*/}
+                                <button
+                                    className="border-0 btn "
+                                    onClick={() => {
+                                        setLoginPopup(true);
+                                    }}
+                                >
+                                    <img
+                                        src={Users}
+                                        width="25"
+                                        alt="Logo"
+                                        className="mr-2 "
+                                    />
+                                </button>
+                                <button className="mx-3 border-0 btn  " to="">
+                                    <img
+                                        src={LogoOut}
+                                        width="25"
+                                        alt="Logo"
+                                        className="mr-2 "
+                                    />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
 
-          {
-            loginPopup && (
-              <LogInSignUp setLoginPopup={setLoginPopup} />
-            ) /*睿渝加的CODE不要刪*/
-          }
-        </Container>
-        {shoppingCart ? <Cart /> : ''}
-      </div>
-    </>
-  );
+                    {
+                        loginPopup && (
+                            <LogInSignUp setLoginPopup={setLoginPopup} />
+                        ) /*睿渝加的CODE不要刪*/
+                    }
+                </Container>
+                <ScrollTo />
+                {shoppingCart ? <Cart /> : ''}
+            </div>
+        </>
+    );
 }
 
 export default Header;
--- src/pages/Products/ProductDetail/index.js
+++ src/pages/Products/ProductDetail/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function index() {
-  return <div>index</div>;
+    return (
+        <>
+            <div className="container">
+                {/* 麵包屑 */}
+                <ul className="breadcrumb products-breadcrumb">
+                    <li className="breadcrumb-item">
+                        <a href="/">首頁</a>
+                    </li>
+                    <li className="breadcrumb-item">
+                        <a href="/">樂器商城 </a>
+                    </li>
+                    <li className="breadcrumb-item">
+                        <a href="/">琴鍵樂器 </a>
+                    </li>
+                    <li className="breadcrumb-item">YAMAHA U系列 U1</li>
+                </ul>
+                {/* 麵包屑 end */}
+            </div>
+        </>
+    );
 }
 
 export default index;
