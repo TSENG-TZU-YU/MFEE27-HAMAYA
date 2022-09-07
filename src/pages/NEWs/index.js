ece99b82fcbc67a65ac837966dd614423b4ba3fa
Lily <lilyhuang841020@gmail.com>
2022-09-07 13:52:28 +0800

    a

--- src/pages/NEWs/index.js
+++ src/pages/NEWs/index.js
@@ -3,6 +3,10 @@ import './index.scss';
 
 import { Link } from 'react-router-dom';
 import NewsBanner from '../../assets/NewsImg/news-banner.jpg';
+import NewsImg from '../../assets/NewsImg/news-img.png';
+import NewsImg2 from '../../assets/NewsImg/news-img2.png';
+import NewsImg3 from '../../assets/NewsImg/news-img3.png';
+import NewsImg4 from '../../assets/NewsImg/news-img4.png';
 
 function NEWs(props) {
     return (
@@ -31,31 +35,108 @@ function NEWs(props) {
 
                 <div className="container">
                     <div className="row">
-                        <div className="col-12 col-md-6">1</div>
                         <div className="col-12 col-md-6">
-                            <div className="">7</div>
-                            <div className="">2</div>
-                            <div className="">9</div>
+                            <img
+                                src={NewsImg}
+                                alt="banner"
+                                width="600"
+                                height="400"
+                                className="img-fluid mt-2"
+                            />
+                            <p className="gary-dark-color h5 News-cursor-pinter mt-3">
+                                【親子點唱機】孟德爾頌《無言歌》為什麼沒有歌詞？
+                            </p>
+                            <div className=" d-flex mt-3 ">
+                                <p className="News-music-article small me-2">
+                                    活動快訊
+                                </p>
+                                <p>李明蒨 － 2022/08/20</p>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-6 News-blank-art-left  ">
+                            <div className="d-flex mt-2 ">
+                                <img
+                                    src={NewsImg2}
+                                    alt="art02"
+                                    width="150"
+                                    height="100"
+                                    className=""
+                                />
+                                <p className="gary-dark-color h5 News-cursor-pinter ms-2 ">
+                                    AI執筆完成貝多芬《第十號交響曲》——AI的創作是否享有著作權法的保護？
+                                    <div className=" d-flex mt-2 ">
+                                        <p className="News-music-article2 small  ">
+                                            音樂文章
+                                        </p>
+                                        <p className="ms-3">
+                                            瓦力 － 2022/08/20
+                                        </p>
+                                    </div>
+                                </p>
+                            </div>
+                            <div className="border-top border-secondary border-1 px-3 pt-3 mt-4 mb-3"></div>
+                            <div className="d-flex mt-2 ">
+                                <img
+                                    src={NewsImg3}
+                                    alt="art02"
+                                    width="150"
+                                    height="100"
+                                    className=""
+                                />
+                                <p className="gary-dark-color h5 News-cursor-pinter ms-2 ">
+                                    廣場上的小提琴手
+                                    <div className=" d-flex mt-2 ">
+                                        <p className="News-music-article3 small  ">
+                                            重要通知
+                                        </p>
+                                        <p className="ms-3">
+                                            眾博法律事務所 － 2022/08/20
+                                        </p>
+                                    </div>
+                                </p>
+                            </div>
+                            <div className="border-top border-secondary border-1 mt-4 mb-3"></div>
+                            <div className="d-flex mt-2 ">
+                                <img
+                                    src={NewsImg4}
+                                    alt="art02"
+                                    width="150"
+                                    height="100"
+                                    className=""
+                                />
+                                <p className="gary-dark-color h5 News-cursor-pinter ms-2 ">
+                                    莫札特《費加洛的婚禮》——聰明，反被聰明誤？
+                                    <div className=" d-flex mt-2 ">
+                                        <p className="News-music-article4 small">
+                                            促銷活動
+                                        </p>
+                                        <p className="ms-3">
+                                            邢子青 － 2022/08/20
+                                        </p>
+                                    </div>
+                                </p>
+                            </div>
+                            <div className="border-top border-secondary border-1  mt-4 mb-3"></div>
                         </div>
                     </div>
                 </div>
-                <div className="container">
-                    <div className=" row text-center ">
-                        <p className="col-3">促銷活動</p>
-                        <p className="col-3">活動快訊</p>
-                        <p className="col-3">重要通知</p>
-                        <p className="col-3">音樂文章</p>
-                    </div>
+            </div>
+            <div className="container">
+                <div className=" row text-center ">
+                    <p className="col-3">促銷活動</p>
+                    <p className="col-3">活動快訊</p>
+                    <p className="col-3">重要通知</p>
+                    <p className="col-3">音樂文章</p>
                 </div>
-                <div className="container">
-                    <div className="row ">
-                        <div className="col-md-4 col-12">8</div>
-                        <div className="col-md-4 col-12">5</div>
-                        <div className="col-md-4 col-12">7</div>
-                        <div className="col-md-4 col-12">10</div>
-                        <div className="col-md-4 col-12">11</div>
-                        <div className="col-md-4 col-12">12</div>
-                    </div>
+            </div>
+            <div className="container">
+                <div className="row ">
+                    <div className="col-md-4 col-12">8</div>
+                    <div className="col-md-4 col-12">5</div>
+                    <div className="col-md-4 col-12">7</div>
+                    <div className="col-md-4 col-12">10</div>
+                    <div className="col-md-4 col-12">11</div>
+                    <div className="col-md-4 col-12">12</div>
                 </div>
             </div>
         </>
