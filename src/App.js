9f113eee947ca1e3b1f34166cc0285a2b9c0e53b
Emmafan <emmafan1114@gmail.com>
2022-09-09 15:56:38 +0800

    Products

--- src/App.js
+++ src/App.js
@@ -54,7 +54,10 @@ function App() {
 
                         <Route path="products" element={<MainOutlet />}>
                             <Route index element={<Products />} />
-                            <Route path=":categoryId" element={<Products />} />
+                            <Route
+                                path="products/:categoryId"
+                                element={<Products />}
+                            />
                             <Route
                                 path=":productId"
                                 element={<ProductDetail />}
