692dda241c9a4f706f1df56efdfdd97eeb1fe78c
lily <lilyhuang841020@gmail.com>
2022-09-12 01:44:21 +0800

    首頁捲軸圖片

---
+++ src/album/home/index.js
@@ -0,0 +1,6 @@
+export const homeImages = [
+    require('./images/slider.jpg'),
+    require('./images/slider2.jpg'),
+    require('./images/slider3.jpg'),
+    require('./images/slider4.jpg'),
+];
