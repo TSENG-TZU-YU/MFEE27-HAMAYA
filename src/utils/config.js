58b835612c3e87fe703e0d20ed20075cfdb1c4fe
RueiYuHuang <b98170022@gmail.com>
2022-09-12 10:32:01 +0800

    fix upload img

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 // REACT_APP_BASE_URL=http://localhost:3002
 //                    http://localhost:3002/api/
 export const API_URL = process.env.REACT_APP_BASE_URL + '/api';
-export const IMAGE_URL = process.env.REACT_APP_BASE_URL;
+export const IMAGE_URL = process.env.REACT_APP_BASE_URL + '/public';
