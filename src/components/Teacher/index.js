c027975d70ae5d5d747cb58188c07941026a60e6
Joy <q39078273@gmail.com>
2022-09-13 20:44:55 +0800

    課程師資進度

--- src/components/Teacher/index.js
+++ src/components/Teacher/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 // 樣式
 import 'swiper/css';
@@ -12,13 +14,13 @@ import { Link } from 'react-router-dom';
 // Swiper
 import { Pagination, Navigation } from 'swiper';
 
-function Teacher(props) {
+function Teacher({ data }) {
     return (
         <div className="d-none d-lg-block">
             <Swiper
                 slidesPerView={5}
-                spaceBetween={30}
-                slidesPerGroup={5}
+                spaceBetween={50}
+                slidesPerGroup={1}
                 loop={true}
                 loopFillGroupWithBlank={true}
                 // 小圓
@@ -31,7 +33,7 @@ function Teacher(props) {
             >
                 {/* 上方圖片 */}
 
-                {props.images.map((item, index) => (
+                {data.map((item, index) => (
                     <SwiperSlide
                         key={index}
                         style={{ width: '200px', height: '100%' }}
@@ -41,13 +43,15 @@ function Teacher(props) {
                             style={{ width: '230px', height: '360px' }}
                         >
                             <img
-                                src={item}
+                                src={require(`../../album/teacher/images/${item.image}`)}
                                 alt="product images"
                                 style={{ width: '100%', height: '300px' }}
                             />
                             <div className="text-center mt-1">
-                                <h5 className="teacher-h5">蔡侑恬 老師</h5>
-                                <p className="teacher-p mt-1">古典鋼琴</p>
+                                <h5 className="teacher-h5">{item.name} 老師</h5>
+                                <p className="teacher-p mt-1 ">
+                                    {item.category}
+                                </p>
                             </div>
                         </Link>
                     </SwiperSlide>
@@ -58,8 +62,8 @@ function Teacher(props) {
 }
 
 /* 要傳進去的圖片資料 */
-Teacher.propTypes = {
-    images: PropTypes.array.isRequired,
-};
+// Teacher.propTypes = {
+//     images: PropTypes.array.isRequired,
+// };
 
 export default Teacher;
