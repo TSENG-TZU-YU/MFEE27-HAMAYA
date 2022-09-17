a00343b0355f4a9ee1b856f4a49fe0f7af9812d5
Joy <q39078273@gmail.com>
2022-09-17 10:46:21 +0800

    1

--- src/pages/Class/Detailed/index.js
+++ src/pages/Class/Detailed/index.js
@@ -30,18 +30,18 @@ function Detailed() {
     const [detailedSelect, setDetailedSelect] = useState(true);
 
     const [data, setData] = useState([]);
-    const [dataAll, setDataAll] = useState([]);
+    const [dataImg, setDataImg] = useState([]);
 
     // 把網址上的 :detailedID 拿出來
     const { detailedID } = useParams();
     console.log('classDetailID', detailedID);
     useEffect(() => {
-        let getAdultClass = async () => {
+        let getClassDetail = async () => {
             let response = await axios.get(
                 `http://localhost:3001/api/class/list/${detailedID}`
             );
             setData(response.data.data);
-            setDataAll(response.data.dataAll.data2.data);
+            setDataImg(response.data.dataImg);
             window.scrollTo({
                 top: 0,
                 left: 0,
@@ -49,8 +49,9 @@ function Detailed() {
             });
             // console.log('data', response.data);
         };
-        getAdultClass();
+        getClassDetail();
     }, []);
+    console.log(dataImg);
 
     useEffect(() => {}, [data]);
 
@@ -99,7 +100,7 @@ function Detailed() {
                                         >
                                             {/* data={data}  */}
                                             {/* images={classImages} */}
-                                            <Carousel images={classImages} />
+                                            <Carousel dataImg={dataImg} />
                                         </div>
                                     </div>
                                 </Col>
@@ -299,7 +300,7 @@ function Detailed() {
                     <div className=" detailed-vector  mt-3 "></div>
                 </div>
                 {/* TODO:  推薦課程  從後端GET 隨機4個課程 */}
-                {dataAll.map((recommend) => {
+                {data.map((recommend) => {
                     return (
                         <div key={recommend.id}>
                             <Row className="mt-5 mb-5 row-cols-xl-4  row-cols-md-2 ">
--- src/pages/Products/ProductDetail/index.js
+++ src/pages/Products/ProductDetail/index.js
@@ -112,6 +112,7 @@ function Product() {
             setShoppingCart([{ ...itemInfo }, ...shoppingCart]);
         }
     }
+    console.log(productImg);
 
     return (
         <>
