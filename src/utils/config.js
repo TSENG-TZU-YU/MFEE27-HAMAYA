be1b07b514e60286119d4c0535bfa733d4216f1e
Steven <a77563166@yahoo.com.tw>
2022-09-29 14:22:05 +0800

    fix 樣式

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,4 +2,3 @@
 //                    http://localhost:3001/api/public/member-1664329052980.jpg
 export const API_URL = process.env.REACT_APP_BASE_URL + '/api';
 export const IMAGE_URL = process.env.REACT_APP_BASE_URL + '/public';
-
