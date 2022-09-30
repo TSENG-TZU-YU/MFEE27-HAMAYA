ed151c69522d8f666a60490fb6be8aa2dee22c87
Emmafan <emmafan1114@gmail.com>
2022-09-30 20:45:15 +0800

    Products

--- src/pages/Products/constants/index.js
+++ src/pages/Products/constants/index.js
@@ -94,15 +94,3 @@ export const breadCrumbCategoryMain = (mainId) => {
             return '';
     }
 };
-
-// loader 動畫
-// export const loader = (
-//     <div className="sk-chase">
-//         <div className="sk-chase-dot"></div>
-//         <div className="sk-chase-dot"></div>
-//         <div className="sk-chase-dot"></div>
-//         <div className="sk-chase-dot"></div>
-//         <div className="sk-chase-dot"></div>
-//         <div className="sk-chase-dot"></div>
-//     </div>
-// );
