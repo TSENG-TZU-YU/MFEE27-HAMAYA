426f2609bfedbcd5c626ba2e25dc156eca2f9f27
RueiYuHuang <b98170022@gmail.com>
2022-09-30 17:48:07 +0800

    update myorderQA

--- src/pages/Member/components/MyOrder/components/MyOrderQA.js
+++ src/pages/Member/components/MyOrder/components/MyOrderQA.js
@@ -7,15 +7,18 @@ import {
     useLocation,
 } from 'react-router-dom';
 import axios from 'axios';
-import { API_URL } from '../../../../../utils/config';
+import { API_URL, IMAGE_URL } from '../../../../../utils/config';
 import { useAuth } from '../../../../../utils/use_auth';
 import { v4 as uuidv4 } from 'uuid';
 import { ReactComponent as Close } from '../../../../../assets/svg/close.svg';
 import { errorToast } from '../../../../../components/Alert';
+import member_img from '../../../../../assets/svg/member_avatar.svg';
+import { MdOutlineAddPhotoAlternate } from 'react-icons/md';
 
 function MyOrderQA(props) {
     const [setbread] = useOutletContext();
     const { socketStatus, setSocketStatus } = useAuth();
+    const [uploadPhotoURL, setUploadPhotoURL] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
     const [myQuestion, setMyQuestion] = useState({
@@ -58,6 +61,7 @@ function MyOrderQA(props) {
             setreplyForm({
                 order_id: response.data.detail.order_id,
                 q_content: '',
+                photo: '',
             });
             setMyQuestion(response.data);
         } catch (err) {
@@ -93,21 +97,34 @@ function MyOrderQA(props) {
     const [replyForm, setreplyForm] = useState({
         order_id: '',
         q_content: '',
+        photo: '',
     });
     const replyFormChange = (e) => {
         setreplyForm({ ...replyForm, q_content: e.target.value });
     };
+    const photoChange = (e) => {
+        setUploadPhotoURL(URL.createObjectURL(e.target.files[0]));
+        setreplyForm({ ...replyForm, photo: e.target.files[0] });
+        // setSelectedPhoto(e.target.files[0]);
+    };
     async function replyFormSubmit(e) {
         e.preventDefault();
         try {
+            let formData = new FormData();
+            formData.append('order_id', replyForm.order_id);
+            formData.append('q_content', replyForm.q_content);
+            formData.append('photo', replyForm.photo);
             let response = await axios.post(
                 `${API_URL}/member/myorder/qareply`,
-                replyForm,
+                formData,
                 {
                     withCredentials: true,
                 }
             );
             //清空replyForm input
+            document.querySelector('.photo').value = '';
+            setreplyForm({ ...replyForm, photo: '' });
+            setUploadPhotoURL('');
             // alert(response.data.message);
         } catch (err) {
             console.log(err.response.data);
@@ -118,26 +135,27 @@ function MyOrderQA(props) {
 
     return (
         <div className="col-12 col-md-8 col-lg-9 mb-3 MyQuestionDetail">
-            <div className="d-flex align-items-center justify-content-between content  my-2">
-                <div>
-                    <h4 className="main-color ">問答詳細</h4>
-                    <div className="">
-                        訂單編號:{myQuestion.order.order_id}&nbsp;
-                        {myQuestion.detail.create_time}
+            <div className="content ">
+                <div className="d-flex align-items-center justify-content-between">
+                    <div>
+                        <h4 className="main-color ">問答詳細</h4>
+                        <div className="">
+                            訂單編號:{myQuestion.order.order_id}&nbsp;
+                            {myQuestion.detail.create_time}
+                        </div>
+                    </div>
+                    <div>
+                        <button
+                            className="closebtn"
+                            onClick={() => {
+                                navigate(-1);
+                            }}
+                        >
+                            <Close />
+                        </button>
                     </div>
                 </div>
-                <div>
-                    <button
-                        className="closebtn"
-                        onClick={() => {
-                            navigate(-1);
-                        }}
-                    >
-                        <Close />
-                    </button>
-                </div>
-            </div>
-            <div className="content ">
+
                 <div className="d-flex border">
                     <div className="col-3 text-center text-light bg-main-color p-1">
                         問題主旨
@@ -185,19 +203,47 @@ function MyOrderQA(props) {
                     <div className="">
                         {myQuestion.content.map((data) => {
                             return (
-                                <div key={uuidv4()}>
-                                    <p className="text-start m-0">
-                                        <span className=" fs-5 fw-bolder">
-                                            {data.name}
-                                        </span>
-                                        &nbsp;
-                                        <span className="">
-                                            {data.create_time}
-                                        </span>{' '}
-                                    </p>
-                                    <p className="text-start fs-6 m-0">
-                                        {data.q_content}
-                                    </p>
+                                <div className="d-flex">
+                                    <div className="imgdiv">
+                                        <img
+                                            src={
+                                                myQuestion.detail.name ===
+                                                data.name
+                                                    ? IMAGE_URL +
+                                                      myQuestion.detail.photo
+                                                    : member_img
+                                            }
+                                            className="img1"
+                                            alt=""
+                                        />
+                                    </div>
+                                    <div key={uuidv4()}>
+                                        <p className="text-start m-0">
+                                            <span className=" fs-5 fw-bolder">
+                                                {data.name}
+                                            </span>
+                                            &nbsp;
+                                            <span className="">
+                                                {data.create_time}
+                                            </span>{' '}
+                                        </p>
+                                        <p className="text-start fs-6 m-0">
+                                            {data.q_content.includes(
+                                                'uploadsQA'
+                                            ) ? (
+                                                <img
+                                                    width={200}
+                                                    src={
+                                                        IMAGE_URL +
+                                                        data.q_content
+                                                    }
+                                                    alt=""
+                                                />
+                                            ) : (
+                                                data.q_content
+                                            )}
+                                        </p>
+                                    </div>
                                 </div>
                             );
                         })}
@@ -214,12 +260,35 @@ function MyOrderQA(props) {
                             placeholder="輸入內容"
                             autoComplete="off"
                         />
-                        <button
-                            className="text-light bg-main-color p-1 px-5 btn1"
-                            onClick={replyFormSubmit}
-                        >
-                            送出
-                        </button>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div>
+                                <label>
+                                    <input
+                                        className="photo d-none"
+                                        type="file"
+                                        name="photo"
+                                        accept="image/png, image/jpeg, image/jpg"
+                                        onChange={photoChange}
+                                    />
+                                    <MdOutlineAddPhotoAlternate className="addimgbtn" />
+                                </label>
+                                {uploadPhotoURL !== '' ? (
+                                    <img
+                                        src={uploadPhotoURL}
+                                        alt=""
+                                        className="addimgmin mx-2"
+                                    />
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                            <button
+                                className="text-light bg-main-color p-1 px-5 btn1"
+                                onClick={replyFormSubmit}
+                            >
+                                送出
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
