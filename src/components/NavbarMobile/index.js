8910dc0b8b0d815dfe90bc3618710f2e23d6d7bf
lily <lilyhuang841020@gmail.com>
2022-10-01 15:16:36 +0800

    update Link

--- src/components/NavbarMobile/index.js
+++ src/components/NavbarMobile/index.js
@@ -176,7 +176,7 @@ function NavbarMobile({ shoppingCart, setShoppingCart }) {
                             <ul className={showSublist}>
                                 <li>
                                     <Link
-                                        to="/"
+                                        to="/news/section?categoryList=1"
                                         className="accent-light-color fw-light text-nowrap"
                                         onClick={closeList}
                                     >
@@ -185,7 +185,7 @@ function NavbarMobile({ shoppingCart, setShoppingCart }) {
                                 </li>
                                 <li>
                                     <Link
-                                        to="/"
+                                         to="/news/section?categoryList=2"
                                         className="accent-light-color fw-light text-nowrap"
                                         onClick={closeList}
                                     >
@@ -194,7 +194,7 @@ function NavbarMobile({ shoppingCart, setShoppingCart }) {
                                 </li>
                                 <li>
                                     <Link
-                                        to="/"
+                                         to="/news/section?categoryList=3"
                                         className="accent-light-color fw-light text-nowrap"
                                         onClick={closeList}
                                     >
